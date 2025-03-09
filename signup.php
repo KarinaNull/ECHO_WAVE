cfb3d55cf5500af8fc6eae2cf8dd2bb0358d3c8c
Karina Nafanova <karinanull@gmail.com>
2025-03-09 18:37:46 +0300

    Add login and registration page markup and styles

---
+++ signup.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Echo Wave</title>
+    <link rel="stylesheet" href="style.css?v=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="welckome-block">
+            <p class="text-welcome">Make some noise. Make some noise. Make some noise. Make some noise. Make some noise.
+            </p>
+        </div>
+        <header>
+            <div class="navbar">
+                <h1>Echo wave</h1>
+                <ul class="nav-list">
+                    <li><a href="#about">About us </a></li>
+                    <li><a href="#catalog">Catalog</a></li>
+                    <li><a href="#contacts">Contacts</a></li>
+                    <a href="./acount.php"><img src="img/Acount.png"></a>
+                </ul>
+            </div>
+        </header>
+
+        <p class="under-head">
+            Make some noise.
+        </p>
+        <div class="sign-in" style="height: 470px;">
+            <img class="electric-gutar-bg-sign" src="img/image.png">
+            <div class="sign-block-left">
+                <div class="block-left-text">
+                    <p class="name-brand">Echo Wave</p>
+                    <p class="slogan">Hear the future</p>
+                    <a href="#">Learn More ⟶</a>
+                </div>
+            </div>
+            <img class="star-sign" src="img/Star 2.png">
+
+
+            <div class="sign-form">
+                <div class="login-container--signup">
+                    <p class="brand-sign">Echo Wave</p>
+                    <p class="sign-title">Sign up</p>
+                    <form>
+                        <label for="email">Email address</label>
+                        <input type="email" id="email" name="email" placeholder="johndoe@email.com" required>
+
+                        <label for="password">Name</label>
+                        <input type="password" id="password" name="password" placeholder="Name" required>
+
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Password" required>
+
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Repeat the password" required>
+
+
+                        <a href="#">Forgot password?</a>
+                        <label>
+                            <input type="checkbox" name="terms"> By selecting Continue, you agree to our Terms of Service and acknowledge our Privacy Policy</a>.
+                        </label>
+
+                        <button type="submit">Continue</button>
+
+                        <hr>
+
+                    </form>
+                    <div class="sign-up-block">
+                        <p>You have account ECHO WAVE? <a class="a-sign" href="sign">Sign in</a></p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <footer>
+            <div class="footer-content">
+                <div class="footer-section-company-info">
+                    <p class="title-footer-section-company-info">NAVIGATION</p>
+                    <ul class="nav-list-footer">
+                        <li><a href="#about">About us </a></li>
+                        <li><a href="#catalog">Catalog</a></li>
+                        <li><a href="#contacts">Contacts</a></li>
+                        <li><a href="#mainPage">Main Page</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-section-social">
+                    <p class="title-footer-section-social">SOCIAL NETWORK</p>
+                    <ul class="nav-list-footer-social">
+                        <li> <img src="img/Facebook.png"> <a href="#">Facebook</a></li>
+                        <li> <img src="img/Linkedin.png"> <a href="#">Linkedin</a></li>
+                        <li> <img src="img/Instagram.png"> <a href="#">Instagram</a></li>
+                        <li> <img src="img/Youtube.png"> <a href="#">Youtube</a></li>
+                        <li> <img src="img/Pintrest.png"> <a href="#">Pintrest</a></li>
+                    </ul>
+                </div>
+
+                <div class="parent-footer"></div>
+                <p class="thnx">Thanks for visiting! We're always happy to help. Contact us if you have any questions.
+                </p>
+
+            </div>
+            <p class="copy">Copyright © 2025 Echo Wave. All rights reserved.</p>
+            <div class="Company">
+                <img src="img/Body.png">
+                <p>Echo Wave</p>
+            </div>
+        </footer>
+    </div>
+
+    <div class="welckome-block">
+        <p class="text-welcome">Make some noise. Make some noise. Make some noise. Make some noise. Make some noise.
+        </p>
+    </div>
+    </div>
+
+
+</body>
+
+</html>
