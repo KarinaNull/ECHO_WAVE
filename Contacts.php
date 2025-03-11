306fb1897fd0b53115e4bbd0987375e0f8aa250d
Karina Nafanova <karinanull@gmail.com>
2025-03-11 22:10:43 +0300

    Added and styled contacts and personal account pages

---
+++ Contacts.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css?v=1.0">
+    <title>Echo Wave</title>
+    <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="welckome-block">
+            <p class="text-welcome">Make some noise. Make some noise. Make some noise. Make some noise. Make some noise.
+            </p>
+        </div>
+        <header>
+            <div class="navbar">
+                <h1>Echo wave</h1>
+                <ul class="nav-list">
+                    <li><a href="#about">About us </a></li>
+                    <li><a href="#catalog">Catalog</a></li>
+                    <li><a href="#contacts">Contacts</a></li>
+                    <a href="./acount.php"><img src="img/Acount.png"></a>
+                </ul>
+            </div>
+        </header>
+
+        <p class="under-head">
+            Make some noise.
+        </p>
+
+        <div class="contacts-form">
+            <div class="contact-text-group">
+                <p class="contact-title">Our contacts</p>
+                <img class="star-contact" src="img/Star 3.png">
+            </div>
+            <p class="contact-text">You can contact us directly or, for example, leave a request so that we can contact you and consider your question or place an order.</p>
+
+            <div class="forms">
+
+                <div class="first-gr">
+                    <div class="name">
+                        <input type="name" id="name" name="name" placeholder="name" required>
+                    </div>
+                    <div class="surname">
+                        <input type="surname" id="surname" name="surname" placeholder="surname" required>
+                    </div>
+                </div>
+
+                <div class="phone">
+                    <input type="phone" id="phone" name="phone" placeholder="Phone" required>
+                </div>
+
+                <div class="question">
+                    <input type="question" id="question" name="question" placeholder="question" required>
+                </div>
+
+                <button type="submit">Send</button>
+
+                <p class="privat">This site is protected by hCaptcha and the hCaptcha Privacy Policy and Terms of Service apply.</p>
+            </div>
+
+            <div class="card-info">
+                <section class="dummy">
+                    <h1>Dummys</h1>
+                    <p>ingo@loremispum.com</p>
+                </section>
+
+                <section class="email">
+                    <h2>Email</h2>
+                    <p>info@loremispum.com</p>
+                    <p>has@loremispum.com</p>
+                </section>
+
+                <section class="phone">
+                    <h2>Phone</h2>
+                    <p>+123 345 678</p>
+                    <p>+123 345 678</p>
+                </section>
+
+                <section class="follow-us">
+                    <h2>Follow us</h2>
+                    <img src="img/Facebook.png">
+                    <img src="img/Linkedin.png">
+                    <img src="img/Instagram.png">
+                    <img src="img/Youtube.png">
+                    <img src="img/Pintrest.png">
+                </section>
+            </div>
+
+            <img class="req-acc--contact" src="img/image.png">
+            <img class="sq-acc--contact" src="img/Mask group (2).png">
+            <img class="stars-acc--contact" src="img/Group 26.png">
+            <p class="personal-acc-test--contact">
+                <span class="adress"> Our store addresses: </span><br><br>
+                123 Main Street, Anytown, CA 91234, USA<br><br>
+                Suite 400, 456 Oak Avenue, Springfield, IL 62704, United States<br><br>
+                The Music Emporium, Building B, 789 Pine Lane, Unit 10, New York, NY 10001, USA
+            </p>
+        </div>
+
+        <footer>
+            <div class="footer-content">
+                <div class="footer-section-company-info">
+                    <p class="title-footer-section-company-info">NAVIGATION</p>
+                    <ul class="nav-list-footer">
+                        <li><a href="#about">About us </a></li>
+                        <li><a href="#catalog">Catalog</a></li>
+                        <li><a href="#contacts">Contacts</a></li>
+                        <li><a href="#mainPage">Main Page</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-section-social">
+                    <p class="title-footer-section-social">SOCIAL NETWORK</p>
+                    <ul class="nav-list-footer-social">
+                        <li> <img src="img/Facebook.png"> <a href="#">Facebook</a></li>
+                        <li> <img src="img/Linkedin.png"> <a href="#">Linkedin</a></li>
+                        <li> <img src="img/Instagram.png"> <a href="#">Instagram</a></li>
+                        <li> <img src="img/Youtube.png"> <a href="#">Youtube</a></li>
+                        <li> <img src="img/Pintrest.png"> <a href="#">Pintrest</a></li>
+                    </ul>
+                </div>
+
+                <div class="parent-footer"></div>
+                <p class="thnx">Thanks for visiting! We're always happy to help. Contact us if you have any questions.
+                </p>
+
+            </div>
+            <p class="copy">Copyright © 2025 Echo Wave. All rights reserved.</p>
+            <div class="Company">
+                <img src="img/Body.png">
+                <p>Echo Wave</p>
+            </div>
+        </footer>
+    </div>
+
+    <div class="welckome-block">
+        <p class="text-welcome">Make some noise. Make some noise. Make some noise. Make some noise. Make some noise.
+        </p>
+    </div>
+    </div>
+</body>
+
+</div>
+
+</body>
+
+</html>
---
+++ personalAccount.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css?v=1.0">
+    <title>Echo Wave</title>
+    <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="welckome-block">
+            <p class="text-welcome">Make some noise. Make some noise. Make some noise. Make some noise. Make some noise.
+            </p>
+        </div>
+        <header>
+            <div class="navbar">
+                <h1>Echo wave</h1>
+                <ul class="nav-list">
+                    <li><a href="#about">About us </a></li>
+                    <li><a href="#catalog">Catalog</a></li>
+                    <li><a href="#contacts">Contacts</a></li>
+                    <a href="./acount.php"><img src="img/Acount.png"></a>
+                </ul>
+            </div>
+        </header>
+
+        <p class="under-head">
+            Make some noise.
+        </p>
+
+        <p class="personal-acc-title">Personal account</p>
+        <img class="star-personal-acc" src="img/Star 2.png">
+
+        <div class="personal-acc-info">
+            <div class="fio">
+                <img src="img/ac.png">
+                <p class="ac-fio">Nafanova Karina Maksimovna</p>
+            </div>
+
+            <div class="fio">
+                <img class="fio-img" src="img/email.png">
+                <p class="ac-fio--email">Email: info@primerkrasoty.ru
+                </p>
+            </div>
+
+        </div>
+        <img class="req-acc" src="img/image.png">
+        <img class="sq-acc" src="img/Mask group (2).png">
+        <img class="stars-acc" src="img/Group 26.png">
+        <p class="personal-acc-test">Welcome to your Echo Wave account! Thank you for being a part of our community. We wish you continued creative success and many musical triumphs!</p>
+
+        <footer>
+            <div class="footer-content">
+                <div class="footer-section-company-info">
+                    <p class="title-footer-section-company-info">NAVIGATION</p>
+                    <ul class="nav-list-footer">
+                        <li><a href="#about">About us </a></li>
+                        <li><a href="#catalog">Catalog</a></li>
+                        <li><a href="#contacts">Contacts</a></li>
+                        <li><a href="#mainPage">Main Page</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-section-social">
+                    <p class="title-footer-section-social">SOCIAL NETWORK</p>
+                    <ul class="nav-list-footer-social">
+                        <li> <img src="img/Facebook.png"> <a href="#">Facebook</a></li>
+                        <li> <img src="img/Linkedin.png"> <a href="#">Linkedin</a></li>
+                        <li> <img src="img/Instagram.png"> <a href="#">Instagram</a></li>
+                        <li> <img src="img/Youtube.png"> <a href="#">Youtube</a></li>
+                        <li> <img src="img/Pintrest.png"> <a href="#">Pintrest</a></li>
+                    </ul>
+                </div>
+
+                <div class="parent-footer"></div>
+                <p class="thnx">Thanks for visiting! We're always happy to help. Contact us if you have any questions.
+                </p>
+
+            </div>
+            <p class="copy">Copyright © 2025 Echo Wave. All rights reserved.</p>
+            <div class="Company">
+                <img src="img/Body.png">
+                <p>Echo Wave</p>
+            </div>
+        </footer>
+    </div>
+
+    <div class="welckome-block">
+        <p class="text-welcome">Make some noise. Make some noise. Make some noise. Make some noise. Make some noise.
+        </p>
+    </div>
+    </div>
+</body>
+
+</html>
